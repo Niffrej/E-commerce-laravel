6c58817ca011b321ae9fbc91be120525df59acd7
Niffrej <francojerffeson@gmail.com>
2025-03-31 20:56:50 -0300

    estrutura incial projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi everyone!</h1>
+      <button>Simple button</button>
+      <Button>bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
